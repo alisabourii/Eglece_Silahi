b55c583c93f50d5cc3c87459b20fca3fb712c41e
Ali <72344723+alisabourii@users.noreply.github.com>
2023-09-03 12:15:56 +0300

    V2

--- son3.ino
+++ son3.ino
@@ -7,30 +7,46 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 Servo srv;
 
+const int echo = 8;
+const int trig = 9;
+
 void setup() {
+  Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.print("hello, world!");
 
   srv.attach(6);
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
 }
 
 void loop() {
+   show();
   int controller = map(analogRead(A0), 0,1023,0,180);
   srv.write(controller);
   if(digitalRead(7) == 0){
     ates();
   }
-  else{
-    OkeyMode();
-  }
 }
 
 void ates(){
+  
   lcd.clear();
   lcd.print("Ates");
 }
 
-void OkeyMode(){
+void show(){
   lcd.clear();
-  lcd.print("OK");
+  lcd.print(Distance());
+  delay(100);
+}
+
+int Distance(){
+  digitalWrite(trig,1);  delay(10);
+  digitalWrite(trig,0);
+
+  int time = pulseIn(echo,1);
+  int dis = (time/2)/28.97;
+  return dis;
 }
